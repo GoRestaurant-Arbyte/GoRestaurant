b6938cbc7cdafee92d0fbf4ae793877d1351164d
Rafael Kammer <rafa.br00@gmail.com>
2020-09-14 20:38:48 -0300

    implementação do POST de login (pendente: validações e url da api no user.js)

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,24 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "../components/Button";
 import TextInput from "../components/TextInput";
+import {login} from "../api/user"
 
 const LoginScreen = ({ history }) => {
+    const [CPF, setCPF] = useState("");
+    const [password, setPassword] = useState("");
+    const doLogin = async () => {
+        try {
+            if (!CPF /* validação de CPF */) {
+                return alert("CPF inválido");
+            }
+            const userData = await login(CPF, password);
+
+            localStorage.setItem("userData", JSON.stringify(userData));
+
+            history.push("/admin");
+        } catch (e) {
+            alert("Deu ruim " + e.message);
+        }
+    };
     return (
         <>
             <TextInput
                 className=""
                 placeholder="CPF"
-                onChangeText={(text) => console.log(text)}
+                onChangeText={(text) => setCPF(text)}
                 onEnterPressed={console.log('Input CPF')}
             />
             <TextInput
                 className=""
                 placeholder="Senha"
-                onChangeText={(text) => console.log(text)}
+                onChangeText={(text) => setPassword(text)}
                 onEnterPressed={console.log('Input senha')}
             />
 
-            <Button onPress={console.log('função de login')}>Entrar</Button>
+            <Button onPress={doLogin}>Entrar</Button>
             <Button onPress={() => history.push("/forgot-pass")}>Recuperar senha</Button>
         </>
     );
