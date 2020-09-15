a379c06032ef1300d03828aa2e33459d202a5986
Phelipe Mazzolli <phelipe.mazzolli@gmail.com>
2020-09-15 14:05:19 -0300

    corrigindo o pull request, subindo input component, e modal de create plates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ModalCreatePlate from "./modals/createPlates"
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="container">
-      <Switch>
-        <Route exact path="/" component={PlatesScreen}></Route>
-        <Route path="/login" component={LoginScreen}></Route>
-        <Route path="/forgot-password" component={ForgotePasswordScreen}></Route>
-      </Switch>
-      <ModalCreatePlate />
+    <div>
     </div>
   );
 }
