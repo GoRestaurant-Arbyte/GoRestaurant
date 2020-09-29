9bffe1e97aa67f9b3132b2d828059c15c44aef31
PH <63376454+Phelipee25@users.noreply.github.com>
2020-09-29 20:01:34 -0300

    Update and rename src/screens/AdminScreen.js to src/telas/AdminScreen.js

--- src/telas/AdminScreen.js
+++ src/telas/AdminScreen.js
@@ -1,8 +1,4 @@
 import React, {useEffect} from "react"
-<<<<<<< HEAD
-=======
-
->>>>>>> 1f2448beae21b8e530baefeaf910a49536ea7c71
 import Header from "../components/header/HeaderLogged"
 import '../components/header/header.css'
 import Card from "../components/card/CardLogged"
